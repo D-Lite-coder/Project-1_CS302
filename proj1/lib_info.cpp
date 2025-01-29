01f39f1b9f8987d4daf8231115748880b8236fcf
D-Lite-coder <davycarp@outlook.com>
2025-01-28 23:48:19 -0500

    Update lib_info.cpp

--- proj1/lib_info.cpp
+++ proj1/lib_info.cpp
@@ -8,142 +8,101 @@
 #include <fstream>
 #include <sstream>
 #include <map>
-#include <string>
-#include <iomanip>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-// Define Song struct
+// Structure to hold song information
 struct Song {
     string title;
-    int duration;  // in seconds
+    string time;
     int track;
-
-    Song(string t, int d, int tr) : title(t), duration(d), track(tr) {}
 };
 
-// Define Album struct
+// Structure to hold album information
 struct Album {
-    string name;
-    map<int, Song> songs; // map track number to Song
-    int songCount;
-    int totalDuration;  // in seconds
-
-    Album(string n) : name(n), songCount(0), totalDuration(0) {}
-
-    // Add a song to the album
-    void addSong(const Song &song) {
-        songs[song.track] = song;
-        songCount++;
-        totalDuration += song.duration;
-    }
+    map<int, Song> songs; // Track number as key
+    int total_songs = 0;
+    int total_seconds = 0;
 };
 
-// Define Artist struct
+// Structure to hold artist information
 struct Artist {
-    string name;
-    map<string, Album> albums;  // map album name to Album
-    int songCount;
-    int totalDuration;  // in seconds
-
-    Artist(string n) : name(n), songCount(0), totalDuration(0) {}
-
-    // Add an album to the artist
-    void addAlbum(const Album &album) {
-        albums[album.name] = album;
-        songCount += album.songCount;
-        totalDuration += album.totalDuration;
-    }
+    map<string, Album> albums; // Album name as key
+    int total_songs = 0;
+    int total_seconds = 0;
 };
 
-// Helper function to replace underscores with spaces
-string replaceUnderscores(string str) {
-    for (size_t i = 0; i < str.length(); i++) {
-        if (str[i] == '_') {
-            str[i] = ' ';
-        }
-    }
-    return str;
-}
-
-// Helper function to convert time (m:ss) to seconds
-int timeToSec(const string &time) {
+// Function to convert time format mm:ss to total seconds
+int timeToSeconds(const string &time) {
     int minutes, seconds;
-    sscanf(time.c_str(), "%d:%d", &minutes, &seconds);
+    char colon;
+    stringstream ss(time);
+    ss >> minutes >> colon >> seconds;
     return minutes * 60 + seconds;
 }
 
-int main(int argc, char *argv[]) {
+// Function to convert seconds to mm:ss format
+string secondsToTime(int total_seconds) {
+    int minutes = total_seconds / 60;
+    int seconds = total_seconds % 60;
+    return to_string(minutes) + ":" + (seconds < 10 ? "0" : "") + to_string(seconds);
+}
 
-    if(argc != 2) {
+// Function to replace underscores with spaces
+string formatString(const string &input) {
+    string formatted = input;
+    replace(formatted.begin(), formatted.end(), '_', ' ');
+    return formatted;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         cerr << "Usage: " << argv[0] << " <filename>" << endl;
-        return 0;
+        return 1;
     }
 
     ifstream file(argv[1]);
-
     if (!file) {
         cerr << "Error opening file: " << argv[1] << endl;
-        return 0;
+        return 1;
     }
 
-    string title, time, artist, album, genre;
-    int track;
-    int timeInSec;
+    map<string, Artist> library;
+    string title, time, artist, album, genre, track;
 
-    // Map to store Artist -> Artist
-    map<string, Artist> artists;
-
-    // Process each line in the file
     while (file >> title >> time >> artist >> album >> genre >> track) {
-        title = replaceUnderscores(title);
-        artist = replaceUnderscores(artist);
-        album = replaceUnderscores(album);
-
-        // Convert time to seconds
-        timeInSec = timeToSec(time);
-
-        // Create a new Song object
-        Song newSong(title, timeInSec, track);
-
-        // Check if the artist exists in the map, otherwise create it
-        if (artists.find(artist) == artists.end()) {
-            artists[artist] = Artist(artist);
-        }
-
-        // Check if the album exists under the artist, otherwise create it
-        if (artists[artist].albums.find(album) == artists[artist].albums.end()) {
-            artists[artist].albums[album] = Album(album);
-        }
-
-        // Add the song to the album
-        artists[artist].albums[album].addSong(newSong);
-
-        // Add the album to the artist
-        artists[artist].addAlbum(artists[artist].albums[album]);
+        string formattedTitle = formatString(title);
+        string formattedArtist = formatString(artist);
+        string formattedAlbum = formatString(album);
+        int trackNum = stoi(track);
+        int songSeconds = timeToSeconds(time);
+        
+        library[formattedArtist].total_songs++;
+        library[formattedArtist].total_seconds += songSeconds;
+        library[formattedArtist].albums[formattedAlbum].total_songs++;
+        library[formattedArtist].albums[formattedAlbum].total_seconds += songSeconds;
+        library[formattedArtist].albums[formattedAlbum].songs[trackNum] = {formattedTitle, time, trackNum};
     }
-
-    // Output the data in the required format
-    for (map<string, Artist>::iterator artistIt = artists.begin(); artistIt != artists.end(); ++artistIt) {
-        // Print artist info
-        cout << artistIt->first << ": " << artistIt->second.songCount << ", " 
-             << artistIt->second.totalDuration / 60 << ":" << setw(2) << setfill('0') 
-             << artistIt->second.totalDuration % 60 << endl;
-
-        // Print albums for each artist
-        for (map<string, Album>::iterator albumIt = artistIt->second.albums.begin(); albumIt != artistIt->second.albums.end(); ++albumIt) {
-            cout << "        " << albumIt->first << ": " << albumIt->second.songCount << ", " 
-                 << albumIt->second.totalDuration / 60 << ":" << setw(2) << setfill('0') 
-                 << albumIt->second.totalDuration % 60 << endl;
-
-            // Print songs for each album
-            for (map<int, Song>::iterator songIt = albumIt->second.songs.begin(); songIt != albumIt->second.songs.end(); ++songIt) {
-                cout << "                " << songIt->first << ". " << songIt->second.title << ": " 
-                     << songIt->second.duration / 60 << ":" << setw(2) << setfill('0') 
-                     << songIt->second.duration % 60 << endl;
+    
+    // Output the sorted data
+    for (map<string, Artist>::iterator artistIt = library.begin(); artistIt != library.end(); ++artistIt) {
+        const string &artistName = artistIt->first;
+        const Artist &artist = artistIt->second;
+        cout << artistName << ": " << artist.total_songs << ", " << secondsToTime(artist.total_seconds) << endl;
+        
+        for (map<string, Album>::const_iterator albumIt = artist.albums.begin(); albumIt != artist.albums.end(); ++albumIt) {
+            const string &albumName = albumIt->first;
+            const Album &album = albumIt->second;
+            cout << "        " << albumName << ": " << album.total_songs << ", " << secondsToTime(album.total_seconds) << endl;
+            
+            for (map<int, Song>::const_iterator songIt = album.songs.begin(); songIt != album.songs.end(); ++songIt) {
+                const Song &song = songIt->second;
+                cout << "                " << song.track << ". " << song.title << ": " << song.time << endl;
             }
         }
     }
-
+    
     return 0;
 }
